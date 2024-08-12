653b146fd53a0b26cd6f421c6eb5daf3cd35694f
Garrett Shipley <garrett.shipley7+github@gmail.com>
2024-08-12 09:14:19 -0400

    Update zzz.R
    Removes the patterns variable that is no longer used

--- R/zzz.R
+++ R/zzz.R
@@ -12,17 +12,3 @@
   )
   stringi::stri_replace_all_regex(string, pattern, "")
 }
-
-
-.patterns <- list(
-  alnum   = "^[\\p{L}\\p{Numeric_Type=Digit}\\p{Numeric_Type=Decimal}\\p{Numeric_Type=Numeric}\\p{Nd}]+$",
-  alpha   = "^\\p{L}+$",
-  ascii   = "^[\\u0000-\\u007F]+$|^$",
-  decimal = "^\\p{Nd}+$",
-  digit   = "^[\\p{Numeric_Type=Digit}\\p{Numeric_Type=Decimal}]+$",
-  lower   = "^\\p{Ll}+$",
-  numeric = "^[\\p{Numeric_Type=Digit}\\p{Numeric_Type=Decimal}\\p{Numeric_Type=Numeric}]+$",
-  space   = "^\\p{WHITE_SPACE}+$",
-  title   = "\\b\\p{Ll}",
-  upper   = "^\\P{CASED}*\\p{Lu}+\\P{CASED}*$"
-)
